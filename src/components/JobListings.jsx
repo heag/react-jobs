8b2964302c9992ac8d8a9695c0028067052e0545
AzureAD\HectorGarcia <hgarcia@globalshares.com>
2025-01-29 15:32:23 +0100

    004 Adapt models to Backend

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,7 +9,7 @@ const JobsListing = ({ isHome = false }) => {
   useEffect(() => {
     const fetchJobs = async () => {
       const apiUrl = isHome
-        ? "/api/jobs?_limit=3"
+        ? "/api/jobs?count=3"
         : "/api/jobs";
       try {
         const res = await fetch(apiUrl);
